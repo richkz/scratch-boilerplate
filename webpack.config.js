7d038ab4468837af7d40461776304d1cd703f6ed
richkz <rich@inventry.com>
2017-12-06 21:23:09 -0600

    html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
 // Using node's native package 'path'
-// 
+// https://nodejs.org/api/path.html
 const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // import our plugin
 
 // constant with our paths
 const paths = {
     DIST: path.resolve(__dirname, 'dist'),
+    SRC: path.resolve(__dirname, 'src'),
     JS: path.resolve(__dirname, 'src/js'),
 };
 
-// Webpack config
+// Webpack configuration
 module.exports = {
     entry: path.join(paths.JS, 'app.js'),
     output: {
         path: paths.DIST,
-        filename: 'app.bundle.js'
-    }
+        filename: 'app.bundle.js',
+    },
+    // Tell webpack to use html plugin
+    // index.html is used as template in which it will inject bundled app.
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(paths.SRC, 'index.html'),
+        }),
+    ],
+    // Dev server configuration
+    // "src" folder used as start point    
 };
 
